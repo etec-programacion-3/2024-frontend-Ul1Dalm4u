e8316f46139e1033135594e0d831e0366ad1dd7e
Santinosabatini <sc.sabatini@alumno.etec.um.edu.ar>
2024-09-25 12:49:34 -0300

    clase 25/9

--- E-commerce/src/Components/Header.jsx
+++ E-commerce/src/Components/Header.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { CiSearch, CiHeart, CiBag1 } from "react-icons/ci";
 import { Link } from 'react-router-dom';
-import './HomePage.css'; // si tienes estilos específicos
+import './HomePage.css'; // Estilos para el header
 
 const Header = () => {
     return (
         <header>
             <p>Perito Moreno 2397, Godoy Cruz</p>
-            <h3 className='title'>ULI _ TINO</h3>
+            <Link to='/' className='title-link'>
+                <h3 className='title'>ULI _ TINO</h3>
+            </Link>
             <div className='top_right_header'>
-                <Link to='/login'><p>Login</p></Link>
-                <CiSearch />
-                <CiHeart /> 
-                <CiBag1 />
+                <Link to='/login' aria-label="Login">
+                    <p>Login</p>
+                </Link>
+                <CiSearch aria-label="Search" />
+                <CiHeart aria-label="Favorites" />
+                <CiBag1 aria-label="Shopping Cart" />
             </div>
         </header>
     );
 };
 
 export default Header;
+
